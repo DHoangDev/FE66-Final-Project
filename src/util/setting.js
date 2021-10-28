3e3b2d3117eac5e65ff9b1ebdbf6749af1666de8
Hoang <hoangntu56@gmail.com>
2021-10-28 12:00:20 +0700

    tmp

--- src/util/setting.js
+++
@@ -1,6 +0,0 @@
-
-export const DOMAIN = 'https://movienew.cybersoft.edu.vn';
-export const GROUP_ID = 'GP01';
-export const USER_LOGIN = 'userLogin';
-export const ACCESS_TOKEN = 'accessToken';
-export const CYBERSOFTTOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNjYiLCJIZXRIYW5TdHJpbmciOiIzMS8wMy8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NDg2ODQ4MDAwMDAiLCJuYmYiOjE2MTc1NTU2MDAsImV4cCI6MTY0ODgzMjQwMH0.x2aYBurEV6HW_u5m4Fhmr7bbp60q1hcW3_KcQ6nrySI';
