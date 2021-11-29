d37710b61b9e272cfe462aa48cd66a75bbc47fc3
Hoang <hoangntu56@gmail.com>
2021-11-29 23:10:18 +0700

    updatePagi

--- src/Pages/AdminTemplate/Pagination/Pagination.js
+++ src/Pages/AdminTemplate/Pagination/Pagination.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-
 import Table from './Table';
 
-import { USER_LOGIN, GROUP_ID } from '../../../Util/Setting';
+import { GROUP_ID } from '../../../Util/Setting';
 import {
     nguoiDungAction,
     nguoiDungPhanTrangAction,
@@ -11,10 +10,8 @@ import {
 
 export default function Pagination() {
 
-    const { arrNguoiDung } = useSelector(state => state.QuanLyNguoiDungReducer);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(10);
     const dispatch = useDispatch();
+    const { arrNguoiDung } = useSelector(state => state.QuanLyNguoiDungReducer);
 
     useEffect(() => {
         const actionNguoiDung = nguoiDungAction(GROUP_ID);
@@ -25,28 +22,6 @@ export default function Pagination() {
         const soPhanTu = document.getElementById('itemNums').value;
         const actionNguoiDungPhanTrang = nguoiDungPhanTrangAction(GROUP_ID, soPhanTu);
         dispatch(actionNguoiDungPhanTrang);
-        setCurrentPage(1)
-    }
-
-    // Current Items
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = arrNguoiDung.slice(indexOfFirstItem, indexOfLastItem)
-
-    // Change Page
-    const paginate = (value) => {
-        setCurrentPage(value)
-    }
-
-    const itemPerPage = (e) => {
-        setItemsPerPage(e.target.value)
-    }
-
-    const search = (dataInput) => {
-        dispatch({
-            type: 'SEARCH_USER',
-            taiKhoan: dataInput,
-        })
     }
 
     return (
@@ -54,35 +29,13 @@ export default function Pagination() {
             <div className="col-md-12">
                 <h3 className="">Quản lý người dùng</h3>
             </div>
-            <div className="col-md-4">
-                <div className="form-group d-flex align-items-center">
-                    <label for="itemNums" className="m-0">Số lượng người dùng muốn lấy :</label>
-                    <input type="text" id="itemNums" className=" form-control form-control-sm mx-2 w-25" />
-                    <button className="btn btn-primary btn-sm" onClick={getData}>Show</button>
-                </div>
+            <div className="col-md-12">
+                <Table
+                    arrNguoiDung={arrNguoiDung}
+                    totalItems={arrNguoiDung.length}
+                    getData={getData}
+                />
             </div>
-            <div className="col-md-4">
-                <div className="form-group">
-                    <select className="custom-select custom-select-sm w-50" onChange={itemPerPage}>
-                        <option value="5">5 / page</option>
-                        <option value="10" selected>10 / page</option>
-                        <option value="20">20 / page</option>
-                        <option value="50">50 / page</option>
-                        <option value="100">100 / page</option>
-                        <option value="200">200 / page</option>
-                        <option value="350">350 / page</option>
-                        <option value="500">500 / page</option>
-                    </select>
-                </div>
-            </div>
-            <Table
-                items={currentItems}
-                itemsPerPage={itemsPerPage}
-                totalItems={arrNguoiDung.length}
-                paginate={paginate}
-                currentPage={currentPage}
-                search={search}
-            />
         </div>
     )
 }
--- src/Pages/AdminTemplate/Pagination/Table.js
+++ src/Pages/AdminTemplate/Pagination/Table.js
@@ -1,21 +1,94 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-export default function Table({ items, itemsPerPage, totalItems, paginate, currentPage, search }) {
+export default function Table({ arrNguoiDung, totalItems, getData }) {
 
-    const pages = 10;
-    const pageNumbers = [];
+    const dispatch = useDispatch();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+    const [disablePre, setDisablePre] = useState('disabled');
+    const [disableNext, setDisableNext] = useState('');
+
     const minPage = 1;
-    const maxPage = Math.ceil(totalItems / itemsPerPage);
-    const [disablePre, setDisablePre] = useState('disabled')
-    const [disableNext, setDisableNext] = useState('')
+    const maxPage = Math.ceil(totalItems / itemsPerPage); // Total page
 
-    for (let i = 1; i <= maxPage; i++) {
-        pageNumbers.push(i)
+    // Current Items
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = arrNguoiDung.slice(indexOfFirstItem, indexOfLastItem)
+
+
+    // Search
+    const search = (dataInput) => {
+        dispatch({
+            type: 'SEARCH_USER',
+            taiKhoan: dataInput,
+        })
+    }
+    const searchPart = () => {
+        return (
+            <div className="col-md-2">
+                <div className="form-group input-group-sm">
+                    <input
+                        className="form-control"
+                        placeholder="Nhập tài khoản ..."
+                        onKeyUp={(e) => {
+                            search(e.target.value);
+                        }}
+                    />
+                </div>
+            </div>
+        )
     }
 
+
+    // Items per page
+    const itemPerPage = (e) => {
+        setItemsPerPage(e.target.value)
+        paginationChange(1)
+    }
+    const selectItemPerPage = () => {
+        return (
+            <div className="col-md-2">
+                <div className="form-group">
+                    <select className="custom-select custom-select-sm w-75" onChange={itemPerPage}>
+                        <option value="5">5 / page</option>
+                        <option value="10" selected>10 / page</option>
+                        <option value="20">20 / page</option>
+                        <option value="50">50 / page</option>
+                        <option value="100">100 / page</option>
+                        <option value="200">200 / page</option>
+                        <option value="350">350 / page</option>
+                        <option value="500">500 / page</option>
+                    </select>
+                </div>
+            </div>
+        )
+    }
+
+
+    // Get items
+    const getItems = () => {
+        return (
+            <div className="col-md-4">
+                <div className="form-group d-flex align-items-center">
+                    <label for="itemNums" className="m-0">Số lượng người dùng muốn lấy :</label>
+                    <input type="text" id="itemNums" className=" form-control form-control-sm mx-2 w-25" />
+                    <button className="btn btn-primary btn-sm"
+                        onClick={() => {
+                            getData();
+                            setCurrentPage(1);
+                        }}>Show</button>
+                </div>
+            </div>
+        )
+    }
+
+
+    // Table
     const renderTable = () => {
-        if (items) {
-            return items.map((value, index) => {
+        if (currentItems) {
+            return currentItems.map((value, index) => {
                 return (
                     <tr key={index}>
                         <td style={{ verticalAlign: 'middle' }}>{value.hoTen}</td>
@@ -31,10 +104,12 @@ export default function Table({ items, itemsPerPage, totalItems, paginate, curre
         } else {
             alert('khong tim thay')
         }
-
     }
+
+
+    // Change Page
     const paginationChange = (value) => {
-        paginate(value)
+        setCurrentPage(value)
         if (value === minPage) {
             setDisablePre('disabled')
             setDisableNext('')
@@ -48,79 +123,109 @@ export default function Table({ items, itemsPerPage, totalItems, paginate, curre
             setDisablePre('')
         }
     }
-
-    const renderPageNumber = () => {
+    const pageNums = () => {
+        let pageNumbers = [];
+        let beforePages = currentPage - 2;
+        let afterPages = currentPage + 2;
+        if (currentPage > minPage) {
+            pageNumbers.push(minPage)
+            if (currentPage > minPage + 3) {
+                pageNumbers.push('...')
+            }
+        }
+        for (let pageLenght = beforePages; pageLenght <= afterPages; pageLenght++) {
+            if (pageNumbers.findIndex(e => e === pageLenght) === -1)
+                pageNumbers.push(pageLenght)
+        }
+        if (currentPage < maxPage) {
+            if (currentPage < maxPage - 3)
+                pageNumbers.push('...')
+            if (pageNumbers.findIndex(e => e === maxPage) === -1)
+                pageNumbers.push(maxPage)
+        }
         return pageNumbers.map((value, index) => {
-            if (currentPage === value) {
+            if (value === currentPage) {
                 return (
-                    <li className="page-item active" key={index}>
-                        <a className="page-link" href="#" onClick={() => paginationChange(value)}>{value}</a>
+                    <li class="page-item active" aria-current="page" key={index}>
+                        <a class="page-link" onClick={() => { paginationChange(value) }} href="#">{value}</a>
                     </li>
                 )
-            } else {
+            }
+            if (value >= minPage && value <= maxPage) {
                 return (
-                    <li className="page-item" key={index}>
-                        <a className="page-link" href="#" onClick={() => paginationChange(value)}>{value}</a>
+                    <li class="page-item" aria-current="page" key={index}>
+                        <a class="page-link" onClick={() => { paginationChange(value) }} href="#">{value}</a>
+                    </li>
+                )
+            }
+            if (value === '...') {
+                return (
+                    <li class="page-item mx-2" aria-current="page" key={index}>
+                        {value}
                     </li>
                 )
             }
         })
     }
+    const paginationButton = () => {
+        if (arrNguoiDung.length <= itemsPerPage) {
+            return (
+                <ul className="pagination pagination-sm">
+                    <li className="page-item disabled">
+                        <a class="page-link">Previous</a>
+                    </li>
+                    {pageNums()}
+                    <li className="page-item disabled">
+                        <a className="page-link">Next</a>
+                    </li>
+                </ul>
+            )
+        }
+        return (
+            <ul className="pagination pagination-sm">
+                <li className={`page-item ${disablePre}`}>
+                    <a class="page-link" href="#" onClick={() => { paginationChange(currentPage - 1) }}>Previous</a>
+                </li>
+                {pageNums()}
+                <li className={`page-item ${disableNext}`}>
+                    <a className="page-link" href="#" onClick={() => { paginationChange(currentPage + 1) }}>Next</a>
+                </li>
+            </ul>
+        )
+    }
 
-    const pre = () => {
-        paginate(currentPage - 1)
-        setDisableNext('')
-        if (currentPage - 1 <= minPage)
-            setDisablePre('disabled')
-    }
-    const next = () => {
-        paginate(currentPage + 1)
-        setDisablePre('')
-        if (currentPage + 1 >= maxPage)
-            setDisableNext('disabled')
-    }
+
 
     return (
-        <div className="col-md-12">
-            <div className="row">
-                <div className="col-md-3">
-                    <input
-                        className="form-control my-2"
-                        placeholder="Nhập tài khoản ..."
-                        onKeyUp={(e) => { search(e.target.value) }}
-                    />
-                </div>
-            </div>
-            <table className="table table-hover table-dark">
-                <thead>
-                    <tr>
-                        <th scope="col">Tên người dùng</th>
-                        <th scope="col">Tài khoản</th>
-                        <th scope="col">Mật khẩu</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Số điện thoại</th>
-                        <th scope="col">Loại người dùng</th>
-                        <th scope="col">Tác vụ</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {renderTable()}
-                </tbody>
-            </table>
-            <div className="row">
-                <div className="col-md-9">
-                    <nav aria-label="Page navigation example">
-                        <ul className="pagination pagination-sm">
-                            <li className={`page-item ${disablePre}`}>
-                                <a class="page-link" onClick={pre} href="#">Previous</a></li>
-                            {renderPageNumber()}
-                            <li className={`page-item ${disableNext}`}>
-                                <a className="page-link" onClick={next} href="#">Next</a></li>
-                        </ul>
+        <div className="row">
+            {searchPart()}
+            {selectItemPerPage()}
+            {getItems()}
+            <div className="col-md-4">
+                <div className="d-flex">
+                    <nav className="ml-auto" aria-label="Page navigation example">
+                        {paginationButton()}
                     </nav>
                 </div>
             </div>
+            <div className="col-md-12">
+                <table className="table table-hover table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">Tên người dùng</th>
+                            <th scope="col">Tài khoản</th>
+                            <th scope="col">Mật khẩu</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Số điện thoại</th>
+                            <th scope="col">Loại người dùng</th>
+                            <th scope="col">Tác vụ</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {renderTable()}
+                    </tbody>
+                </table>
+            </div>
         </div>
-
     )
 }
